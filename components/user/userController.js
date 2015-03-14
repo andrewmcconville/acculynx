a9bcca2ac68dd241dc5c8cc83e1059928afd6f06
Andrew McConville <andrew@andrewmcconville.com>
2015-03-14 17:09:44 -0500

    styles user view animation. adds debug mode

--- components/user/userController.js
+++ components/user/userController.js
@@ -1,11 +1,15 @@
 angular
 	.module('accuchallenge')
 	.controller('userCtrl', ['$scope', '$http', '$stateParams', 'UserID', function($scope, $http, $stateParams, UserID) {
-		$http
-			.get('https://api.stackexchange.com//2.2/users/' + UserID.id + '?order=desc&sort=reputation&site=stackoverflow')
-			.success(function(data, status, headers, config) {
-				$scope.user = data.items[0];
-			})
-			.error(function(data, status, headers, config) {
-			});
+		// $http
+		// 	.get('https://api.stackexchange.com//2.2/users/' + UserID.id + '?order=desc&sort=reputation&site=stackoverflow')
+		// 	.success(function(data, status, headers, config) {
+		// 		$scope.user = data.items[0];
+		// 	})
+		// 	.error(function(data, status, headers, config) {
+		// 	});
+
+		$scope.user = {
+			display_name: 'Andrew'
+		};
 	}]);
