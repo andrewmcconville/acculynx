ba10c396e468fedb2cd81d23a74ced47870c6797
Andrew McConville <andrew@andrewmcconville.com>
2015-03-15 21:02:33 -0500

    adds back button for user view

--- shared/js/app.js
+++ shared/js/app.js
@@ -4,25 +4,16 @@ app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider,
 	$urlRouterProvider.otherwise('/questions');
 
 	$stateProvider
-		/*
-		 * Home
-		 */
 		.state('questions', {
 			url: '/questions',
 			templateUrl: 'components/questions/questionsTemplate.html',
         	controller: 'questionsCtrl'
 		})
-		/*
-		 * Profile
-		 */
 		.state('questions.user', {
 			url: '/user/:id',
 			templateUrl: 'components/user/userTemplate.html',
         	controller: 'userCtrl'
 		})
-		/*
-		 * About
-		 */
 		.state('about', {
 			url: '/about',
 			templateUrl: 'components/about/aboutTemplate.html'
